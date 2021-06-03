926823b289147dd7437ce3d0fbcc59f08d70ed8a
caf-3 <tomas.caetano@ubi.co.mz>
2021-06-03 13:07:55 +0200

    memorystore added

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8808;
 app.use(cors());
 app.use(express.json());
-app.use(session({resave: true, saveUninitialized: true, secret: '73kj%7Jk&k@#sdjf', cookie: { maxAge: 6000}}));
+app.use(session({
+    resave: false, 
+    saveUninitialized: true, 
+    secret: '73kj%7Jk&k@#sdjf', 
+    cookie: { maxAge: 60000},
+    store: new MemoryStore({
+        checkPeriod: 60000
+    })
+}));
 
 const userRoutes = require('./app/router/user');
 const sessionRoutes = require('./app/router/session');
